fb3300000a1130eea4908fdbdf4c8121a0895a00
gbbgabriel <gabrielbarruecobotelho10@gmail.com>
2023-07-06 17:15:16 -0300

    add mapper

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,18 +1,31 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { ReturnUserDto } from './dtos/returnUser.dto';
+import { Entity } from 'typeorm';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UsePipes(ValidationPipe)
   @Post()
-  async create(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
+  async create(@Body() createUserDto: CreateUserDto): Promise<ReturnUserDto> {
     return this.userService.createUser(createUserDto);
   }
 
   @Get()
-  async findAllUser(): Promise<UserEntity[]> {
-    return this.userService.findAllUser();
+  async findAllUser(): Promise<ReturnUserDto[]> {
+    return (await this.userService.findAllUser()).map(
+      (userEntity) => new ReturnUserDto(userEntity),
+    );
   }
 }
