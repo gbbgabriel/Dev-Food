be5fe507db8508b2e48ee924aeb929fc4bb78b33
gbbgabriel <gabrielbarruecobotelho10@gmail.com>
2023-07-17 05:16:57 -0300

    create productService

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ProductEntity } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ReturnProduct } from './dtos/return-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private readonly productRepository: Repository<ProductEntity>,
+  ) {}
+
+  async findAllProducts(): Promise<ReturnProduct[]> {
+    const products = await this.productRepository.find();
+
+    if (!products || products.length === 0) {
+      throw new NotFoundException('Not found products');
+    }
+
+    return products.map((products) => new ReturnProduct(products));
+  }
+}
